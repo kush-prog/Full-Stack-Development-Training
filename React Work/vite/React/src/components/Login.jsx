1dc63aa7c644f8d98e4d2ab77a3ee3c2dcd6d7db
kush-prog <comedykid041@gmail.com>
2024-12-03 12:43:43 +0530

    ---

--- React Work/vite/React/src/components/Login.jsx
+++ React Work/vite/React/src/components/Login.jsx
@@ -4,6 +4,18 @@ const Login = () => {
   return (
     <div>
       <h2>Welcome to Login Page</h2>
+
+      <form>
+        <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+        </div>
+        <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
