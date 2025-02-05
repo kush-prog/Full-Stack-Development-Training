cd78b4ce40907421389b6e0b08721aac74cb2a16
kush-prog <chauhankush12345@gmail.com>
2025-02-05 09:00:43 +0530

    Previous  work

--- React Work/vite/React/src/App.jsx
+++ React Work/vite/React/src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css';
+import { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import Login from './components/Login';
 import Registration from './components/Registration';
 // import Dashboard from './components/Dashboard';
 function App() {
-  
+  const [store, setStore] = useState("");
+
+
   // return (
     // <div className='app'>
     //   <State/>
@@ -17,13 +20,15 @@ function App() {
 
   return (
     <div>
+      {JSON.stringify(store)}
       <BrowserRouter>
         
         <Routes>
 
           <Route path="/" element={<MainLayout/>}>
             <Route path="/login" element={<Login/>}/>
-            <Route path="/registration" element={<Registration/>}/>
+            <Route path="/registration" element={<Registration  regData={setStore}/>}/>
+            
           </Route>
         
         </Routes>
--- React Work/vite/React/src/components/Registration.jsx
+++ React Work/vite/React/src/components/Registration.jsx
@@ -1,32 +1,81 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+function Registration({regData}) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  function getData(e){
+    e.preventDefault();
+    const data = {
+      name, email, password
+    }
 
-const Registration = () => {
+    regData(data);
+
   return (
     <div>
-      Welcome to Registration Page
-
+      <h2>Welcome to Registration Page</h2>
+      
       <form>
-      <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Name</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Name"/>
+        
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Name</label>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter Name"
+          />
         </div>
         <div className="form-group">
-            <label htmlFor="exampleInputEmail1">Email address</label>
-            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="Enter email"
+          />
         </div>
         <div className="form-group">
-            <label htmlFor="exampleInputPassword1">Password</label>
-            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Password"
+          />
         </div>
         <div className="form-check">
-            <input type="checkbox" className="form-check-input" id="exampleCheck1"/>
-            <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+          <input
+            type="checkbox"
+            className="form-check-input"
+            id="exampleCheck1"
+          />
+          <label className="form-check-label" htmlFor="exampleCheck1">
+            Check me out
+          </label>
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button onClick={getData} type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      
       </form>
+    
     </div>
-  )
+  );
+};
 }
 
-export default Registration
+export default Registration;
